a8a3f16a14bf74753855133ff359ddb1ea68daf1
unknown <andersonsilver18@gmail.com>
2023-03-21 11:31:04 -0300

    Atualizando

--- index.js
+++ index.js
@@ -38,21 +38,28 @@ let BD = {
     ]
 }
 
+// chama todo o conteudo do banco de dados falso BD e imprime na tela
+
 app.get("/games",(rew,res) =>{
     res.statusCode = 200;
     res.json(BD.games);
 })
+
+// chama do banco de dado falso "BD" pelo ID e imprime na tela.
 
 app.get("/games/:id",(req,res)=>{
     res.statusCode = 200;
 
     if(isNaN(req.params.id)){
         res.sendStatus = 400;
+        // Caso der erro retorna erro statusCOde 400
     }else{
         
         let id = parseInt(req.params.id);
 
         let game = BD.games.find(g => g.id == id);
+
+
             if(game != undefined){
                 res.statusCode = 200;
                 res.json(game)
@@ -65,10 +72,11 @@ app.get("/games/:id",(req,res)=>{
 app.post("/games", (req,res) =>{
 
     var {title, price, year} = req.body;
+    let aletório = Math.floor(Math.random() * 100)
 
     BD.games.push({
 
-        id: 10,
+        id:aletório,
         title,
         price,
         year
@@ -96,7 +104,6 @@ app.delete("/games/:id", (req,res) =>{
 })
 
 app.put("/games/:id", (req,res)=>{
-    res.statusCode = 200;
 
     if(isNaN(req.params.id)){
         res.sendStatus = 400;
